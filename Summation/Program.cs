3b3ab0ef97d716dd42a633c6f798460168d5f5b3
Osama1010C <osama.smsma999@gmail.com>
2024-07-24 22:44:49 +0300

    summation problem

---
+++ Summation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number Of Numbers : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum = "+Sum(arr));
+            
+        }
+        static int Sum(int[] arr)
+        {
+            int result = 0;
+            foreach (int i in arr)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
