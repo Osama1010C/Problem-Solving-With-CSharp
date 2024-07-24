39202be72fcefa1f902ac7797232eac9d3a9e3c5
Osama1010C <osama.smsma999@gmail.com>
2024-07-24 22:50:55 +0300

    new

---
+++ Max Split/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Max_Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int slize = 1;
+            for(int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == 'R' && s[i+1] == 'L') slize++;
+            }
+            Console.WriteLine(slize);
+
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == 'R' && s[i+1] == 'L')
+                {
+                    Console.Write(s[i]);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(s[i]);
+                }
+            }
+            Console.Write(s[s.Length - 1]);           
+        }
+    }
+}
