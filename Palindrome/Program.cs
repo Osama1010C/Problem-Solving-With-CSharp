205db366b0a9d548cb374d03e767188715eb9893
Osama1010C <osama.smsma999@gmail.com>
2024-07-31 18:04:15 +0300

    new problems from leetcode

---
+++ Palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(10));
+        }
+        public static bool IsPalindrome(int x)
+        {
+            string tmp = x + "";
+            for(int i = 0; i < tmp.Length; i++)
+            {
+                if (tmp[i] != tmp[tmp.Length-1-i]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Two Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = TwoSum(new int[] { 3, 2, 4 }, 6);
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
