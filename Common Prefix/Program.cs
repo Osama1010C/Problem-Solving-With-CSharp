60e30d682b791a2f9f331e11f7ad07bbe16f12c1
Osama1010C <osama.smsma999@gmail.com>
2024-07-31 21:18:20 +0300

    it was baaaaaaaad problem

---
+++ Common Prefix/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Common_Prefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestCommonPrefix(new string[] { "reflower", "flow", "flight" }));
+            Console.WriteLine(LongestCommonPrefix(new string[] {  "a", "a", "b" }));
+            
+
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string result = "";
+
+            for(int i = 0; i < SmallestLength(strs); i++)
+            {
+                if (CommonIndexOfChar(strs, i))
+                {
+                    result += strs[0][i];
+                }
+                else break;
+            }
+
+            return result;
+        }
+
+        public static bool CommonIndexOfChar(string[] s, int index)
+        {
+            for(int i = 0; i < s.Length-1; i++)
+            {
+                if(s[i][index] != s[i + 1][index]) return false;
+            }
+            return true;
+        }
+        
+        public static int SmallestLength(string[] strs)
+        {
+            int len = strs[0].Length;
+            for(int i = 1; i <strs.Length; i++)
+            {
+                if(len > strs[i].Length) len = strs[i].Length;
+            }
+            return len;
+        }   
+        
+    }
+}
