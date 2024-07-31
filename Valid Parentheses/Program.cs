51097f7adae93b3042b0c98ab1fa896fe81c2150
Osama1010C <osama.smsma999@gmail.com>
2024-07-31 22:39:03 +0300

    using stack

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("([])"));
+        }
+
+        public static bool IsValid(string s)
+        {
+            if(s.Length % 2 != 0) return false;
+
+            Stack<char> stack = new Stack<char>();
+            foreach(char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' && stack.Count != 0 && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else if (c == ']' && stack.Count != 0 && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else if (c == '}' && stack.Count != 0 && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
