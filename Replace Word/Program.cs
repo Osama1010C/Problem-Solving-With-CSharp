799ea817b4f9bd6858c6f33311bdc6ff3a3966a6
Osama1010C <osama.smsma999@gmail.com>
2024-08-02 18:21:06 +0300

    #

---
+++ Replace Word/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Replace_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(Replace(s));
+        }
+        public static string Replace(string s)
+        {   
+            string result = "";
+            for (int i = 0; i< s.Length; i++)
+            {
+                if(i < s.Length - 4)
+                {
+                    if (s[i] == 'E' && s[i + 1] == 'G' && s[i + 2] == 'Y' && s[i + 3] == 'P' && s[i + 4] == 'T')
+                    {
+                        result += " ";
+                        i += 4;
+                    }
+                    else result += s[i];
+                }
+                else result += s[i];
+            }
+
+            return result;
+        }
+    }
+}
