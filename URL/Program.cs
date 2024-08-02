82f4300fcf4f6fd86877850d972f6e80574adc58
Osama1010C <osama.smsma999@gmail.com>
2024-08-02 17:47:30 +0300

    good one

---
+++ URL/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace URL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            GetParameters(s);
+        }
+        public static void GetParameters(string s)
+        {
+            int index = IndexOfChar(s, '?');
+            for(int i = index + 1; i < s.Length; i++)
+            {
+                if (s[i] == '=') Console.Write(": ");
+                else if (s[i] == '&') Console.Write("\n");
+                else Console.Write(s[i]);
+            }
+        }
+        public static int IndexOfChar(string s, char c)
+        {
+            for(int i = 0; i <= s.Length; i++)
+            {
+                if (s[i] == c) return i;
+            }
+            return -1;
+        }
+    }
+}
