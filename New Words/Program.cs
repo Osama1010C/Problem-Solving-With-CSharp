1f41761706770ecc8d8fe51b8762a636e9a3eb19
Osama1010C <osama.smsma999@gmail.com>
2024-08-02 18:01:38 +0300

    #

---
+++ New Words/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace New_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(HowMany(s));
+        }
+        public static int HowMany(string s)
+        {
+            s=s.ToLower();
+            int result = 0;
+            int[] recurrence = new int[5];
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'e') recurrence[0]++;
+                if (s[i] == 'g') recurrence[1]++;
+                if (s[i] == 'y') recurrence[2]++;
+                if (s[i] == 'p') recurrence[3]++;
+                if (s[i] == 't') recurrence[4]++;
+
+            }
+            if (IsEquals(recurrence)) result = recurrence[0];
+            else result = MinOfArray(recurrence);
+
+            return result;
+        }
+        public static bool IsEquals(int[] array)
+        {
+            int first = array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if(array[i] != first) return false;
+            }
+            return true;
+        }
+
+        public static int MinOfArray(int[] array)
+        {
+            int min = array[0];
+            for(int i = 1;i < array.Length; i++)
+            {
+                if(min > array[i]) min = array[i];
+            }
+            return min;
+        }
+    }
+}
