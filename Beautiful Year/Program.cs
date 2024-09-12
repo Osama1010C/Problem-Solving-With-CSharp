12a9bbf3ac342e8cd23fbe3110f6a6b8750f1f3d
Osama1010C <osama.smsma999@gmail.com>
2024-09-12 23:30:43 +0300

    #

---
+++ Beautiful Year/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Beautiful_Year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            string? result = null;
+
+            while (true)
+            {
+                year++;
+                string strYear = year + "";
+                if (IsUnique(strYear))
+                {
+                    result = strYear;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        static bool IsUnique(string str)
+        {
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    if (str[i] == str[j]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
