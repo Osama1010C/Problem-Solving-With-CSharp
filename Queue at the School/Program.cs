f8b7287f53d92e1d296fe82eff5ecd0a0c503852
Osama1010C <osama.smsma999@gmail.com>
2024-09-13 00:07:10 +0300

    #

---
+++ Queue at the School/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Queue_at_the_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputs = Console.ReadLine();
+            string[] arrinputs = inputs.Split(' ');
+            int n = Convert.ToInt32(arrinputs[0]);
+            int second = Convert.ToInt32(arrinputs[1]);
+            string s = Console.ReadLine();
+
+            for(int i = 0; i < second; i++)
+            {
+                for(int j = 0; j < s.Length-1; j++)
+                {
+                    if (s[j] == 'B' && s[j+1] == 'G') { s = Swap(s, j, j + 1); j++; }
+                }
+            }
+            Console.WriteLine(s);
+        }
+        static string Swap(string s, int firstIndex, int secondIndex)
+        {
+            char[] chars = s.ToCharArray();
+            char tmp = chars[firstIndex];
+            chars[firstIndex] = chars[secondIndex];
+            chars[secondIndex] = tmp;
+            return new string(chars);
+        }
+    }
+}
