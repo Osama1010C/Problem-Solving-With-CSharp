6e3adf3f8cd7fbda162e618eeb0091e802b1b97d
Osama1010C <osama.smsma999@gmail.com>
2024-09-12 18:55:22 +0300

    #

---
+++ Translation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Translation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Console.WriteLine(Process(a, b) == true ? "YES" : "NO");
+            
+        }
+
+        static bool Process(string a, string b)
+        {
+            if (a.Length != b.Length)  return false;
+            for (int i = 0, j = a.Length-1; i < a.Length; i++, j--)
+            {
+                if (a[i] != b[j]) return false;
+            }
+            return true;
+        }
+    }
+}
