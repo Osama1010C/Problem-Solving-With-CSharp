18a02a2c6602705891a5f73ccb3af9203b4746ec
Osama1010C <osama.smsma999@gmail.com>
2024-09-12 18:44:25 +0300

    new problem

---
+++ Anton and Danik/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Anton_and_Danik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            int anton = 0, danik = 0;
+            foreach (char c in s)
+            {
+                if (c == 'A') anton++;
+                else danik++;
+            }
+            Console.WriteLine(anton == danik ? "Friendship" : anton > danik ? "Anton" : "Danik");
+        }
+    }
+}
