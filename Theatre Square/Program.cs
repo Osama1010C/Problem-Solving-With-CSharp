2db55f95d0fcbef1da1e0543b5da0fa0011fe6d9
Osama1010C <osama.smsma999@gmail.com>
2024-09-14 01:08:10 +0300

    #

---
+++ Theatre Square/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Theatre_Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] results = line.Split(' ');
+            long n = int.Parse(results[0]);
+            long m = int.Parse(results[1]);
+            long a = int.Parse(results[2]);
+            long x = m/a, y = n/a;
+            if (m % a != 0) x++;
+            if (n % a != 0) y++;
+            Console.WriteLine(x*y);
+        }
+    }
+}
