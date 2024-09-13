db904fe5b69525bc0a78890f0fb04c927d32f7ba
Osama1010C <osama.smsma999@gmail.com>
2024-09-13 17:26:40 +0300

    #

---
+++ In Search of an Easy Problem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace In_Search_of_an_Easy_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] sinputs = s.Split(' ');
+            bool isHard = false;
+            foreach (string num in sinputs)
+            {
+                if(num == "1")
+                {
+                    isHard = true;
+                    break;
+                }
+            }
+            Console.WriteLine(isHard ? "HARD":"EASY");
+        }
+    }
+}
