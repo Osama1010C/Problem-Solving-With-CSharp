6ea0c62a3befba4524072db4964195ccf007ff6d
Osama1010C <osama.smsma999@gmail.com>
2024-09-13 17:54:52 +0300

    #

---
+++ Magnets/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Magnets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr = new string[n];
+            for (int i = 0; i < arr.Length; i++) arr[i] = Console.ReadLine();
+
+            string tmp = arr[0];
+            int groups = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] != tmp) groups++;
+                tmp = arr[i];
+            }
+            Console.WriteLine(groups);
+        }
+    }
+}
