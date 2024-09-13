0e37d292075d6f2da563fd765e92509d162f63a3
Osama1010C <osama.smsma999@gmail.com>
2024-09-13 17:36:15 +0300

    #

---
+++ George and Accommodation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace George_and_Accommodation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfRooms = Convert.ToInt32(Console.ReadLine());
+            string[] rooms = new string[numOfRooms];
+            int count = 0;
+            for (int i = 0; i < numOfRooms; i++)
+            {
+                rooms[i] = Console.ReadLine();
+                string[] line = rooms[i].Split(' ');
+                int p = int.Parse(line[0]);
+                int q = int.Parse(line[1]);
+                if (p + 2 <= q) count++;
+            }
+            Console.WriteLine(count);   
+        }
+    }
+}
