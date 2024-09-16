6899f07af9e70db161070a99a6f3357dd0702619
Osama1010C <osama.smsma999@gmail.com>
2024-09-16 19:03:10 +0300

    #

---
+++ Drinks/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Drinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string line = Console.ReadLine();
+            string[] results = line.Split(' ');
+            double sum = 0;
+            foreach (string s in results)
+            {
+                int num = int.Parse(s);
+                sum += num;
+            }
+            Console.WriteLine(sum/n);
+        }
+    }
+}
