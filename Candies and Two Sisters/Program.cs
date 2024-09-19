5a144a17a97c11654f7faf0ff70ec4bb808c52e2
Osama1010C <osama.smsma999@gmail.com>
2024-09-19 16:09:38 +0300

    #

---
+++ Candies and Two Sisters/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Candies_and_Two_Sisters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            while(testCases-- > 0)
+            {
+                long number = Convert.ToInt32(Console.ReadLine());
+                decimal equation = (number-1) / 2;
+                decimal count = Math.Floor(equation);
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
