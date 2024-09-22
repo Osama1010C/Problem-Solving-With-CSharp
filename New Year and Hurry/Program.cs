96c4a9539a53140b8c9eec0fc18936b08d44c6c5
Osama1010C <osama.smsma999@gmail.com>
2024-09-22 11:17:35 +0300

    #

---
+++ New Year and Hurry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Schema;
+
+namespace New_Year_and_Hurry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // total minutes is 240
+            string[] line = Console.ReadLine().Split(' ');
+            int n = int.Parse(line[0]);
+            int k = int.Parse(line[1]);
+            int solvedProblems = 0;
+            int total = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if (total + k > 240) break;
+                total += 5 * i;
+                solvedProblems+= total+k > 240 ? 0: 1;
+            }
+            Console.WriteLine(solvedProblems);
+        }
+    }
+}
