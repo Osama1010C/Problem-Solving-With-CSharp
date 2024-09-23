47b7440052f82bf46e06fd9dc07eefaf58c3faa2
Osama1010C <osama.smsma999@gmail.com>
2024-09-23 21:52:57 +0300

    #

---
+++ Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while(t-- > 0)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                int a = Convert.ToInt32(line[0]), b = Convert.ToInt32(line[1]), c = Convert.ToInt32(line[2]);
+                Console.WriteLine((a + b == c) || (a + c == b) || (b + c == a) ? "YES":"NO");
+            }
+        }
+    }
+}
