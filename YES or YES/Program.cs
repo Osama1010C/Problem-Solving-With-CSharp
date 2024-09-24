f8cf03d4b1b9c21634faf43087ac6d021a5f4de2
Osama1010C <osama.smsma999@gmail.com>
2024-09-24 16:02:10 +0300

    #

---
+++ YES or YES/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace YES_or_YES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while(t-- > 0)
+            {
+                string line = Console.ReadLine().ToLower();
+                Console.WriteLine(line == "yes" ? "YES" : "NO");
+            }
+        }
+    }
+}
