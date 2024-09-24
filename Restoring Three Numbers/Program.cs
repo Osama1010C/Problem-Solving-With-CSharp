a08f2872864e751089ed40801419a8c348a0694c
Osama1010C <osama.smsma999@gmail.com>
2024-09-24 15:57:55 +0300

    #

---
+++ Restoring Three Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Restoring_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int x1 = int.Parse(line[0]);
+            int x2 = int.Parse(line[1]);
+            int x3 = int.Parse(line[2]);
+            int x4 = int.Parse(line[3]);
+            int[] x = { x1, x2, x3, x4 };
+            int AblusBblusC = Max(x);
+            for(int i = 0; i < 4; i++)
+            {
+                Console.Write(AblusBblusC - x[i] == 0 ? "": AblusBblusC - x[i] + " ");
+            }
+        }
+        static int Max(int[] x)
+        {
+            int max = x[0];
+            foreach (int i in x)
+                if (i > max) max = i;
+            return max;
+        }
+    }
+}
