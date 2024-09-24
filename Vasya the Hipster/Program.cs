bf0783798e13eba099cb3d60a921e2bfb4a7f0a1
Osama1010C <osama.smsma999@gmail.com>
2024-09-24 15:30:15 +0300

    #

---
+++ Vasya the Hipster/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Vasya_the_Hipster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+
+            int NumOfDaysWithDifferentSocks = a < b ? a : b;
+            int NumOfDaysWithSameSocks = a < b ? (b-a)/2 : (a-b)/2;
+            Console.WriteLine($"{NumOfDaysWithDifferentSocks} {NumOfDaysWithSameSocks}");
+        }
+    }
+}
