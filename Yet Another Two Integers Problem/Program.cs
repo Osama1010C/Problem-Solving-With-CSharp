b78401604b9c444d41cf6303543295f3c8f6a9db
Osama1010C <osama.smsma999@gmail.com>
2024-09-25 16:08:12 +0300

    #

---
+++ Yet Another Two Integers Problem/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Yet_Another_Two_Integers_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while(t-- > 0)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                int num1 = int.Parse(line[0]);
+                int num2 = int.Parse(line[1]);
+                int result = Math.Abs(num1-num2);
+                if (num1 == num2) { Console.WriteLine(0); continue; }
+                else Console.WriteLine(NumberOfMoves(result));
+            }
+        }
+        static int NumberOfMoves(int n)
+        {
+            if ((n + "").Length == 1 || n == 10) return 1;
+            return n%10 == 0 ? (n/10) : (n / 10) + 1;
+        }
+    }
+}
